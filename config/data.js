ad68d422aedbcd89dbf4cfce11128073f27bacab
Javi <arancibiajav@gmail.com>
2018-12-16 13:50:56 -0500

    m

--- config/data.js
+++ config/data.js
@@ -1,7 +1,7 @@
 {
 	context:{
 		defaultCurrentPage:'page_about_us',
-		API_URL:'https://utopiaecoaldea.herokuapp.com',
+		API_URL:'https://utopiaecoaldea.herokuapp.com'
 	},
 	login:{
 		code: 'cm5y'
