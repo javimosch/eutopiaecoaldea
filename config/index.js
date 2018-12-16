741b63f6af234800356a3e0f943cdfd78ba9c1a4
Javi <arancibiajav@gmail.com>
2018-12-16 13:43:55 -0500

    m

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config({
-	silent:true
+	silent: true
 });
 const dJSON = require('dirty-json');
 
@@ -7,37 +7,37 @@ var locales = require('./locales.js');
 var self = module.exports = {
 	defaultOutputFolder: 'docs',
 	NODE_ENV: process.env.NODE_ENV,
-	API_URL: process.env.API_URL,
 	defaultLanguage: 'es',
-	context:{
-		defaultCurrentPage:'page_ecoaldea',
+	context: {
+		defaultCurrentPage: 'page_ecoaldea',
 		currentPage: 'page_ecoaldea',
 		NODE_ENV: process.env.NODE_ENV,
 		API_URL: process.env.API_URL
 	},
-	getContext: function(language){
+	getContext: function(language) {
 		//console.log('getConfig',{language})
 		language = language || self.defaultLanguage;
-		function collectLanguage(language){
+
+		function collectLanguage(language) {
 			var lang = {};
-			Object.keys(locales).forEach(setenceKey=>{
+			Object.keys(locales).forEach(setenceKey => {
 				var sentenceObject = locales[setenceKey]
 				var sentenceValue = '';
-				if(sentenceObject[language]){
-					sentenceValue= sentenceObject[language];
-				}else{
-					if(sentenceObject[self.defaultLanguage]){
-						sentenceValue= sentenceObject[self.defaultLanguage];	
-					}else{
-						sentenceValue= setenceKey;
+				if (sentenceObject[language]) {
+					sentenceValue = sentenceObject[language];
+				} else {
+					if (sentenceObject[self.defaultLanguage]) {
+						sentenceValue = sentenceObject[self.defaultLanguage];
+					} else {
+						sentenceValue = setenceKey;
 					}
 				}
-				lang[setenceKey]= sentenceValue;
+				lang[setenceKey] = sentenceValue;
 			});
 			//console.log('LANG',language,JSON.stringify(lang,null,4))
 			return lang;
 		}
-		var result = Object.assign({},self.context,{
+		var result = Object.assign({}, self.context, {
 			lang: collectLanguage(language)
 		});
 		result.currentLanguage = result.lang[language];
@@ -47,16 +47,30 @@ var self = module.exports = {
 };
 
 
-var data = require('sander').readFileSync(__dirname+'/data.js').toString('utf-8');
-var savedData  = {}
-try{
+var data = require('sander').readFileSync(__dirname + '/data.js').toString('utf-8');
+var savedData = {}
+try {
 	savedData = dJSON.parse(data);
-	
-}catch(err){
+
+} catch (err) {
 	console.error('config: invalid data')
 }
-Object.assign(self, savedData || {});
-Object.assign(self,{
-	API_URL: process.env.API_URL||self.API_URL
-});
+//Object.assign(self, savedData || {});
+merge(self,savedData);
+function merge(self, savedData) {
+	if(savedData===undefined){
+		return;
+	}
+	Object.keys(self).forEach(k => {
+		if (typeof self[k] === 'object' && !(self[k] instanceof Array)) {
+			merge(self[k],savedData[k]);
+		} else {
+			self[k] = savedData[k] || self[k];
+		}
+	});
+}
 
+
+Object.assign(self.context, {
+	API_URL: process.env.API_URL || self.context.API_URL
+});
