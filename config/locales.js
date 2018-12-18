40c513c3b4da3bd89c0c7c7096b3a930961c1e90
javimosch <arancibiajav@gmail.com>
2018-12-18 15:09:04 -0500

    Translation groups

--- config/locales.js
+++ config/locales.js
@@ -43,15 +43,22 @@ module.exports = {
 		en:"Products and fairs",
 		fr:"Produits et marchés"
 	},
+		PRODUCTOS_FERIAS_INTRO:{
+		es:"Feria de productos biologicos y locales !                Ven a nuestra aldea para conocer productores responsables, probar                sus productos deliciosos y disfrutar del fin de semana lejos del                estress de la ciudad !",
+		en:"Organic and local products fair ! Come to our eco-center to discover local producers who respect nature, try their delicious products and enjoy being in the countryside, surrounded by nature !",
+		fr:"Marché de produits bio et locaux ! Venez visiter notre centre écologique pour découvrir des producteurs responsables, goûter leurs délicieux produits et profiter d'une journée dans la nature !"
+	},
 		FERIAS_PRODUCTORES_CONTENT_1:{
 		es:"Estamos comprometidos en ayudar a los productores locales vender su producción a precio justo en el mercado local para ofrecer lo mejor a la población de Chiclayo y alrededores.<br>        Cada 2 fines de semana, organizamos en la aldea una gran feria de productos organicos en las cuales participan una variedad de productores locales.<br>        Encontrarás yogures, quesos, miel, frutas, verduras, bebidas refrescantes, todo natural y organico !",
 		en:"We are engaged in helping local producers to sell their production for a fair price to the local market, to offer the best of our local products to the inhabitants of Chiclayo and the nearby areas. <br> One in two week-end, we organize a fair with all the producers where we sell a large diversity of local products.<br> You will find honey, yogurt, cheese, fruits, vegetables, fresh drinks, all natural and organic !",
 		fr:"Nous nous engageons auprès des producteurs locaux pour les aider à vendre leur production à un prix juste sur le marché local, afin que la population de Chiclayo et alentours puisse profiter de nos délicieux produits régionaux.<br>Un week-end sur deux, nous organisons un grand marché des producteurs pour que chacun puisse venir vendre ses produits <br> Vous y trouverez du miel, des yaourts artisanaux, des fromages, des fruits et légumes, des boissons fraîches, toujours d'origine naturelle et écologique bien-sûr !"
-	},FERIAS_PRODUCTORES_CONTENT_2:{
+	},
+	FERIAS_PRODUCTORES_CONTENT_2:{
 		es:"Aprovecha visitarnos en estas ferias para disfrutar de nuestro eco-centro y pasar un día en familia lejos del estress de la ciudad.",
 		en:"Come and visit us for the fairs and enjoy our eco-center to spend a family day away from the city !",
 		fr:"Venez découvrir notre marché et ses producteurs, et profitez-en pour passer la journée en famille dans notre centre écologique, loin du stress de la ville !"
-	},FERIAS_PRODUCTORES_CONTENT_3:{
+	},
+	FERIAS_PRODUCTORES_CONTENT_3:{
 		es:"Proponemos una zona de juego, piscina, zona de descanso, biblioteca.<br>                    Eco-restaurante con productos frescos y orgánicos cocinados en el momento.<br>                    Talleres y actividades variadas para aprender a toda edad sobre las plantas, la cerámica, la pintura o también practicar la meditación, el yoga y el tai-chi !<br>                    Visita guiada de nuestro vivero, la huerta, el centro de tratamiento de los desechos.<br>                    Escenario con música en vivo y otros espectaculos (ver programacion)<br>                    Zona de camping<br>                    Y mucho más !",
 		en:"We offer all the facilities for you to have a great day : playground, swimming pool, relaxing area, reading area.<br>                    Eco-restaurant with fresh ecological product homecooked.<br>                    Workshops to learn and discover at any age about nature, crops, ceramic, painting, or to practice meditation, yoga, or Tai-chi !<br>                    Visits with explainations about our garden, plant nursery, compost and waste treatment area.<br>                    Stage with live music and a diversity of shows (see our programation)<br>                    Camping zone<br>                    And much more ! !",
 		fr:"Nous vous proposons tous les services pour une journée réussie : aire de jeux, piscine, zone de détente, bibliotèque.<br>                    Eco-restaurant avec des produits frais et biologiques cuisinés sur place.<br>                    Ateliers et activités pour apprendre à tout age sur les plantes, la céramique, la peinture ou pour pratiquer la méditation, le yoga ou le tai-chi!<br>                    Visite guidée de notre jardin, pépinière, compost et du centre de traitement des déchets.<br>                    Scène avec de la musique en live et une offre variée de spectacles (voir programmation)<br>                    Aire de camping<br>                   Et beaucoup plus !"
@@ -105,6 +112,16 @@ module.exports = {
 		es:"Grupos",
 		en:"Groups",
 		fr:"Groupes"
+	},
+	GRUPOS_INTRO:{
+		es:"Familia, escuela, empresa, organiza sus eventos en nuestra aldea !",
+		en:"Families, schools, companies, organize your events in our eco-center !",
+		fr:"Familles, écoles, entreprises, organisez vos évenements dans notre centre écologique !"
+	},
+	GRUPOS_CONTENT_1:{
+		es:"Proponemos un panel de actividades, visitas y talleres para diseñar eventos a su medida, siempre en el respeto de nuestros valores, aprendiendo, compartiendo y disfrutando en la naturaleza para momentos inolvidables !",
+		en:"We offer a large range of activities, visits, workshops to create on-demand events customized just for you ! Always looking forward to share experiences, learn and enjoy the nature for unforgettable moments !",
+		fr:"Nous proposons un large choix d'activités, de visites guidées, d'ateliers participatifs afin de créer des événements sur mesure, toujours dans le respect de nos valeurs, en profitant de la nature, dans le partage et l'apprentissage pour des moments inoubliables !"
 	},
 	HEADER_HOME_LABEL:{
 		es:"Descubrir",
