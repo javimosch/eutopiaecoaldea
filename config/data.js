3a80aff0a0daac5f4313adc31ec2517c964403e5
robot <noreply@robot.com>
2018-12-18 14:00:31 +0000

    pushPath commit

--- config/data.js
+++ config/data.js
@@ -2,7 +2,7 @@
     "context": {
         "defaultCurrentPage": "page_about_us",
         "API_URL": "https://utopiaecoaldea.herokuapp.com",
-        "updateCode": "j2ko-w2GL"
+        "updateCode": "97ZNZnbEC"
     },
     "login": {
         "code": "cm5y"
