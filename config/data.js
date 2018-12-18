c37ffca8e4d28364b4ab80d1cec442a24c2b333b
robot <noreply@robot.com>
2018-12-18 13:39:49 -0500

    pushPath commit

--- config/data.js
+++ config/data.js
@@ -1,39 +1,11 @@
 {
-	"context": {
-		wipMode: false,
-		"defaultCurrentPage": "page_ecoaldea",
-		"PUBLIC_URL":"https://eutopiaecoaldea.com",
-		"API_URL": "https://utopiaecoaldea.herokuapp.com",
-		"updateCode": "gtdB1Q27s",
-		programacion: [{
-				fecha: "04-04-2018",
-				eventos: [{
-					image: "/img/flor-permacultura.webp",
-					title: "La danza de la flor",
-					message: "El proximo sabado 04 de Abril, ven a descubrir la danza de la flor, grupo de musica local. Tambien estara disponible el Eco-restaurant.",
-					time: "16:30"
-				}]
-			},{
-				fecha:"07-10-2018",
-				eventos:[{
-					image:"/img/logo.png",
-					title:"Brainstorming Eutopia",
-					message:"En Octubre recibimos a cientificos de la NASA para pensar juntos en el futuro de la tierra.",
-					time: "10:00"
-				}]
-			}
-
-		]
-	},
-	"login": {
-		"code": "cm5y"
-	},
     "context": {
         "defaultCurrentPage": "page_about_us",
         "API_URL": "https://utopiaecoaldea.herokuapp.com",
-        "updateCode": "iwDxvgaR2"
+        "updateCode": "B69AkNwgU"
     },
     "login": {
         "code": "cm5y"
-    }
+    },
+    "wipMode": false
 }
