eee5700211707079a854a43f49d34be6f4145061
robot <noreply@robot.com>
2018-12-19 11:01:01 -0500

    pushPath commit

--- config/data.js
+++ config/data.js
@@ -15,8 +15,16 @@
                         "title": "La danza de la flor",
                         "message": "El proximo sabado 04 de Abril, ven a descubrir la danza de la flor, grupo de musica local. Tambien estara disponible el Eco-restaurant.",
                         "time": "16:30"
+                    },
+                    {
+                        "image": "/img/logo.png",
+                        "title": "Brainstorming Eutopia",
+                        "message": "En Octubre recibimos a cientificos de la NASA para pensar juntos en el futuro de la tierra.",
+                        "time": "10:00"
                     }
-                ]
+                ],
+                "_expand": false,
+                "show": true
             },
             {
                 "fecha": "07-10-2018",
@@ -27,7 +35,9 @@
                         "message": "En Octubre recibimos a cientificos de la NASA para pensar juntos en el futuro de la tierra.",
                         "time": "10:00"
                     }
-                ]
+                ],
+                "_expand": false,
+                "show": true
             }
         ],
         "voluntarios": [
