34fc18071078a600750afbbb1619a9fe3c4fe418
robot <noreply@robot.com>
2018-12-19 11:07:25 -0500

    pushPath commit

--- config/data.js
+++ config/data.js
@@ -5,45 +5,56 @@
         "PUBLIC_URL": "https://eutopiaecoaldea.com",
         "API_URL": "https://utopiaecoaldea.herokuapp.com",
         "PREVIEW_URL": "https://utopiaecoaldea.herokuapp.com",
-        "updateCode": "66zorq5sd",
-        "programacion": [{
-            "fecha": "04-04-2018",
-            "eventos": [{
-                "image": "/img/flor-permacultura.webp",
-                "title": "La danza de la flor 2",
-                "message": "El proximo sabado 04 de Abril, ven a descubrir la danza de la flor, grupo de musica local. Tambien estara disponible el Eco-restaurant.",
-                "time": "16:30"
-            }, {
-                "image": "",
-                "title": "Plantas carnivoras",
-                "message": "Vamos a plantar 500 plantas",
-                "time": "09:00"
-            }, {
-                "image": "/img/logo.png",
-                "title": "Brainstorming Eutopia",
-                "message": "En Octubre recibimos a cientificos de la NASA para pensar juntos en el futuro de la tierra.",
-                "time": "10:00"
-            }],
-            "_expand": false,
-            "show": true
-        }, {
-            "fecha": "07-10-2018",
-            "eventos": [{
-                "image": "/img/logo.png",
-                "title": "Brainstorming Eutopia",
-                "message": "En Octubre recibimos a cientificos de la NASA para pensar juntos en el futuro de la tierra.",
-                "time": "10:00"
-            }],
-            "_expand": false,
-            "show": true
-        }],
-        "voluntarios": [{
-            "name": "Pepe Alfonso Ramirez",
-            "email": "pepe@asd.com",
-            "whyMessage": "Por el BosqueEscuela",
-            "helpMessage": "Eco-quinta",
-            "dateMessage": "Verano"
-        }]
+        "updateCode": "7PUTgddiL",
+        "programacion": [
+            {
+                "fecha": "04-04-2018",
+                "eventos": [
+                    {
+                        "image": "/img/flor-permacultura.webp",
+                        "title": "La danza de la flor 2",
+                        "message": "El proximo sabado 04 de Abril, ven a descubrir la danza de la flor, grupo de musica local. Tambien estara disponible el Eco-restaurant.",
+                        "time": "16:30"
+                    },
+                    {
+                        "image": "",
+                        "title": "Plantas carnivoras",
+                        "message": "Vamos a plantar 500 plantas",
+                        "time": "09:00"
+                    },
+                    {
+                        "image": "/img/logo.png",
+                        "title": "Brainstorming Eutopia",
+                        "message": "En Octubre recibimos a cientificos de la NASA para pensar juntos en el futuro de la tierra.",
+                        "time": "10:00"
+                    }
+                ],
+                "_expand": false,
+                "show": true
+            },
+            {
+                "fecha": "07-10-2018",
+                "eventos": [
+                    {
+                        "image": "/img/logo.png",
+                        "title": "Brainstorming Eutopia",
+                        "message": "En Octubre recibimos a cientificos de la NASA para pensar juntos en el futuro de la tierra.",
+                        "time": "10:00"
+                    }
+                ],
+                "_expand": false,
+                "show": true
+            }
+        ],
+        "voluntarios": [
+            {
+                "name": "Pepe Alfonso Ramirez",
+                "email": "pepe@asd.com",
+                "whyMessage": "Por el BosqueEscuela",
+                "helpMessage": "Eco-quinta",
+                "dateMessage": "Verano"
+            }
+        ]
     },
     "login": {
         "code": "cm5y"
