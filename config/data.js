85330cc496c5d76a44e85ebe9485a8fbdc9f58c6
robot <noreply@robot.com>
2018-12-19 08:10:21 -0500

    pushPath commit

--- config/data.js
+++ config/data.js
@@ -28,6 +28,15 @@
                     }
                 ]
             }
+        ],
+        "voluntarios": [
+            {
+                "name": "pepe",
+                "email": "pepe@asd.com",
+                "whyMessage": "",
+                "helpMessage": "",
+                "dateMessage": ""
+            }
         ]
     },
     "login": {
