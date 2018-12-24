3e0de4a4b372f2fafb961781b7b45d066ad0490a
robot <noreply@robot.com>
2018-12-24 22:14:24 +0000

    pushPath commit

--- config/data.js
+++ config/data.js
@@ -5,7 +5,7 @@
         "PUBLIC_URL": "https://eutopiaecoaldea.com",
         "API_URL": "https://utopiaecoaldea.herokuapp.com",
         "PREVIEW_URL": "https://utopiaecoaldea.herokuapp.com",
-        "updateCode": "rGA7Jgd-u",
+        "updateCode": "vECvfrWFs",
         "programacion": [
             {
                 "fecha": "04-04-2018",
