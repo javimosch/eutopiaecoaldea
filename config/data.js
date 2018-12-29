f3e668e0fc149c7b82819b65667c2e2fbfc1f0c6
robot <noreply@robot.com>
2018-12-29 06:26:34 +0000

    pushPath commit

--- config/data.js
+++ config/data.js
@@ -1,6 +1,6 @@
 {
     "context": {
-        "wipMode": true,
+        "wipMode": false,
         "defaultCurrentPage": "page_ecoaldea",
         "PUBLIC_URL": "https://eutopiaecoaldea.com",
         "API_URL": "https://utopiaecoaldea.herokuapp.com",
